b112a4429bc7f98b2e3fe0a3f073844575449dbb
Marcus Chong <21354757+Mizusake@users.noreply.github.com>
2020-11-02 18:01:22 +0800

    updated background at mainmenu

--- MainMenu.c
+++ MainMenu.c
@@ -3,9 +3,12 @@
 void main_menu_init(void)
 {
 	CP_Settings_Background(CP_Color_Create(0, 0, 0, 255));
-	CP_Graphics_DrawRect(450, 100, 800, 200);
+	//CP_Graphics_DrawRect(450, 100, 800, 200);
+	CP_Image_Draw(CP_Image_Load("Assets/mainMenu/background.jpg"), 850, 450, (float)CP_System_GetWindowWidth(), (float)CP_System_GetWindowHeight(), 255);
+	CP_Image_Draw(CP_Image_Load("Assets/mainMenu/title.png"), 850, 200, 1000, 300, 255);
 	CP_Image_Draw(CP_Image_Load("Assets/mainMenu/startButton.png"), 850, 450, 300, 150, 255);
 	CP_Image_Draw(CP_Image_Load("Assets/mainMenu/exitButton.png"), 850, 650, 300, 150, 255);
+	//CP_Graphics_DrawRect(450, 100, 800, 200);
 	//CP_Graphics_DrawRect(700, 400, 300, 150);
 	//CP_Graphics_DrawRect(700, 600, 300, 150);
 }
