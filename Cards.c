ed1b3c3e2cc5d71f559be62ac22391621228aa94
Marcus Chong <21354757+Mizusake@users.noreply.github.com>
2020-11-03 23:43:30 +0800

    Cards.c & Cards.h

---
+++ Cards.c
@@ -0,0 +1,33 @@
+#include "cprocessing.h"
+#include <stdio.h>
+
+typedef struct CardType {
+	char type;
+	int ret;
+	int mana;
+} CardType;
+
+CardType attackCard(void) {
+	CardType cards;
+	cards.type = 'a';
+	cards.ret = 3;
+	cards.mana = 2;
+	return cards;
+}
+
+CardType defenceCard(void) {
+	CardType cards;
+	cards.type = 'd';
+	cards.ret = 2;
+	cards.mana = 1;
+	return cards;
+}
+
+CardType healCard(void) {
+	CardType cards;
+	cards.type = 'h';
+	cards.ret = 3;
+	cards.mana = 1;
+	return cards;
+} 
+
---
+++ Cards.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct CardType {
+	char type;
+	int ret;
+	int mana;
+} CardType;
+
+CardType attackCard(void);
+CardType defenceCard(void);
+CardType healCard(void)
