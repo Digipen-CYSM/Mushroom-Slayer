9b6d0a154338591c512372c8f2bcdfdc7d03d1e0
Yijian <sj_yijian@hotmail.com>
2020-11-09 23:28:54 +0800

    Update Gameover

--- GameOver.c
+++ GameOver.c
@@ -1,4 +1,5 @@
 #include "cprocessing.h"
+#include "MainMenu.h"
 
 void game_over_init(void)
 {
@@ -11,6 +12,12 @@ void game_over_update(void)
 	CP_Settings_TextSize(300);
 	CP_Settings_Fill(CP_Color_Create(255, 0, 0, 255));
 	CP_Font_DrawText("Game Over!", 450, 450);
+	CP_Image_Draw(CP_Image_Load("Assets/mainMenu/Exit.png"), 850, 630, 500, 170, 255);
+	CP_Image_Draw(CP_Image_Load("Assets/mainMenu/exitButton.png"), 850, 750, 300, 150, 255);
+	if (CP_Input_KeyTriggered(KEY_ESCAPE))
+	{
+		CP_Engine_SetNextGameState(main_menu_init, main_menu_update, main_menu_exit);
+	}
 }
 
 void victory_update(void)
@@ -18,6 +25,12 @@ void victory_update(void)
 	CP_Settings_TextSize(300);
 	CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
 	CP_Font_DrawText("Victory!", 450, 450);
+	CP_Image_Draw(CP_Image_Load("Assets/mainMenu/Exit.png"), 850, 630, 500, 170, 255);
+	CP_Image_Draw(CP_Image_Load("Assets/mainMenu/exitButton.png"), 850, 750, 300, 150, 255);
+	if (CP_Input_KeyTriggered(KEY_ESCAPE))
+	{
+		CP_Engine_SetNextGameState(main_menu_init, main_menu_update, main_menu_exit);
+	}
 }
 
 void game_over_exit(void)
