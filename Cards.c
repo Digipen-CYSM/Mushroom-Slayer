a68416de9a92b091d43adec9301a8f91db832c67
Yijian <sj_yijian@hotmail.com>
2020-11-15 23:23:53 +0800

    Revert "Merge branch 'master' of https://github.com/Digipen-CYSM/Mushroom-Slayer"
    This reverts commit dd661cdee42c60a60d583f0dd0f41b87f0b81006, reversing
    changes made to affd1aadaa8712f37899811bda34c3b00c57b861.

--- Cards.c
+++ Cards.c
@@ -1,14 +1,6 @@
 #include "cprocessing.h"
 #include <stdio.h>
 #include "Cards.h"
-CardType hand[50];
-CardType* deckPtr;
-CardType* handPtr;
-CardType deck[50];
-int handRng[50], rCheck = 1, oneToTen;
-CP_Image deckSrc[50];
-
-
 CardType attackCard(void) {
 	CardType cards;
 	cards.type = 'a';
@@ -18,7 +10,6 @@ CardType attackCard(void) {
 	cards.baseSrc = "Assets/cards/attackBase.png";
 	cards.valSrc = "Assets/cards/val3.png";
 	cards.manaSrc = "Assets/cards/mana2.png";
-	cards.imgSrc = "Assets/cards/attackBase.png";
 	return cards;
 }
 
@@ -31,55 +22,18 @@ CardType defenceCard(void) {
 	cards.baseSrc = "Assets/cards/defenceBase.png";
 	cards.valSrc = "Assets/cards/val2.png";
 	cards.manaSrc = "Assets/cards/mana1.png";
-	cards.imgSrc = "Assets/cards/defenceBase.png";
 	return cards;
 }
 
+CardType healCard(void) {
+	CardType cards;
+	cards.type = 'h';
+	cards.ret = 3;
+	//cards.ret = CP_Random_RangeInt(1, 3)
+	cards.mana = 1;
+	cards.baseSrc = "Assets/cards/healBase.png";
+	cards.valSrc = "Assets/cards/val3.png";
+	cards.manaSrc = "Assets/cards/mana1.png";
+	return cards;
+} 
 
-
-CardType* generateDeck(void) {
-	//generate cards into deck
-	for (int i = 0; i < 10; i++) {
-		if (i < 5) {
-			deck[i] = attackCard();
-		}
-		else {
-			deck[i] = defenceCard();
-		}
-	}
-	deckPtr = deck;
-	return deckPtr;
-}
-
-CardType* drawCards(CardType* deckp,int numCards,int numDeck) {	
-	for (int i = 0; i < numCards; i++) {
-		while (rCheck == 1) {
-			rCheck = 0;
-			oneToTen = CP_Random_RangeInt(0, numDeck);
-			for (int j = 0; j < numCards; j++) {
-				if (handRng[j] == oneToTen && i != 0) {
-					rCheck = 1;
-				}
-			}
-		}
-		handRng[i] = oneToTen;
-		hand[i] = deckp[oneToTen];
-	}
-
-	return handPtr;
-}
-
-void loadDeckImg(CardType* deckp,int numDeck) {
-	for (int i=0; i < numDeck; i++) {
-		deckSrc[i] = CP_Image_Load(deckp[i].imgSrc);
-	}
-}
-
-void drawHandSrc(int numHand) {
-	float cardWidth = 350;
-	for (int i = 0; i < numHand; i++) {
-		//take note need to specify what i is
-		CP_Image_Draw(deckSrc[i], cardWidth, 750, 150, 230, 255);
-		cardWidth += 135;
-	}
-}
--- Cards.h
+++ Cards.h
@@ -7,12 +7,8 @@ typedef struct CardType {
 	char *valSrc;
 	char *baseSrc;
 	char *manaSrc;
-	char *imgSrc;
 } CardType;
 
 CardType attackCard(void);
 CardType defenceCard(void);
-void loadDeckImg(CardType* deckp, int numDeck);
-CardType* generateDeck(void);
-CardType* drawCards(CardType* deck, int numCards, int numDeck);
-void drawHandSrc(int numHand);
+CardType healCard(void);
